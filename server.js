7b6a2ac4f773243e83e0420b005431f86ca437ee
eungbin <programmer97@naver.com>
2024-12-07 21:10:21 +0900

    Add post-note-api

--- server.js
+++ server.js
@@ -20,6 +20,18 @@ app.get('/note', (req, res) => {
   })
 });
 
+app.post('/note', (req, res) => {
+  const { title, content } = req.body;
+  if(!title || !content) return res.status(400).json({ error: 'title 및 content는 필수 항목입니다.' })
+  const sql = "insert into note(title, content) values(?, ?)";
+
+  conn.query(sql, [title, content], (err, result) => {
+    if(err) return res.status(500).json({ error: "데이터 삽입 실패" });
+
+    res.status(201).json({ message: "노트 추가 성공" });
+  })
+})
+
 app.listen(port, () => {
   console.log(`server is listening at localhost:${port}`);
 });
