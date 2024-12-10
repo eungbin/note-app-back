b2c35ffb10bbc3af6d256e435005d899ef0d6a0b
eungbin <programmer97@naver.com>
2024-12-10 22:36:34 +0900

    Add note-update-api and note-delete-api

--- server.js
+++ server.js
@@ -30,6 +30,28 @@ app.post('/note', (req, res) => {
 
     res.status(201).json({ message: "노트 추가 성공" });
   })
+});
+
+app.delete('/note', (req, res) => {
+  const { id } = req.body;
+  const sql = 'delete from note where id=?';
+
+  conn.query(sql, [id], (err, result) => {
+    if(err) return res.status(500).json({ error: "데이터 삭제 실패" });
+
+    res.status(201).json({ message: "노트 삭제 성공" });
+  })
+});
+
+app.put('/note', (req, res) => {
+  const { id, title, content } = req.body;
+  const sql = 'update note set title=?, content=? where id=?';
+
+  conn.query(sql, [title, content, id], (err, result) => {
+    if(err) return res.status(500).json({ error: "데이터 변경 실패" });
+
+    res.status(201).json({ message: "노트 변경 성공" });
+  })
 })
 
 app.listen(port, () => {
